b30ad188fd5ac4565131425c8f1d13ed6633651c
meme <meme_me2019@outlook.com>
2020-04-21 12:13:03 +0800

    重构 test translator 逻辑测试

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import unittest
 from bin_terminal_translater import core, setting
+import requests
 
 
 class Main_test(unittest.TestCase):
@@ -12,8 +13,18 @@ class Main_test(unittest.TestCase):
 
     def test_translator(self):
         r_text = 'Hello'
-        b_text = core.translator(setting, r_text, 'en')
-        self.assertEqual(r_text, b_text)
+        lang_code = 'zh-Hans'
+
+        # 使用核心内函数进行翻译请求
+        t1 = core.translator(setting, r_text, lang_code)
+
+        # 使用同样的参数配置手动请求
+        dtb = setting.data_table.copy()
+        dtb["data"]["text"] = r_text
+        dtb["data"]["to"] = lang_code
+        t2 = requests.post(**dtb).json()[0]['translations'][0]['text']
+
+        self.assertEqual(t1, t2)
 
 
 if __name__ == "__main__":
