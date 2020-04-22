211444de42e5669519f972260b908904576a0fcd
meme <meme_me2019@outlook.com>
2020-04-23 04:14:40 +0800

    添加help信息

--- functional_test.py
+++ functional_test.py
@@ -12,6 +12,8 @@ class FunctionlTest(unittest.TestCase):
         pass
 
     def test_of_main(self):
+        # help
+        entrance(['-h'])
         # 翻译你好到英文
         argv = ['-l', 'en', '你好']
         print(entrance(argv))
@@ -25,6 +27,8 @@ class FunctionlTest(unittest.TestCase):
         # 没有指定文本时从剪贴板获取文本
         argv = ['-l', 'zh-Hans']
         print(entrance(argv))
+
+        #
 
 
 if __name__ == "__main__":
