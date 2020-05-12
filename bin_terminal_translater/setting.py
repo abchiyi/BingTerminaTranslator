449a7be9f1d7ae583b514a9aaca7b271970dc708
meme <meme_me2019@outlook.com>
2020-05-13 02:23:21 +0800

    移除配置文件夹

--- bin_terminal_translater/setting.py
+++ bin_terminal_translater/setting.py
@@ -4,8 +4,8 @@ BASE_DIR_INSIDE = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR_OUTSIDE = os.path.dirname(BASE_DIR_INSIDE)
 
 
-CONF_PATH = os.path.join(BASE_DIR_INSIDE, 'ini/conf.ini')
+CONF_PATH = os.path.join(BASE_DIR_OUTSIDE, 'ini/conf.ini')
 
-CONF_PARSER = os.path.join(BASE_DIR_INSIDE, 'ini/parser.ini')
+CONF_PARSER = os.path.join(BASE_DIR_OUTSIDE, 'ini/parser.ini')
 
-LANGUAGE_CODE_PATH = os.path.join(BASE_DIR_INSIDE, 'ini/language_code.ini')
+LANGUAGE_CODE_PATH = os.path.join(BASE_DIR_OUTSIDE, 'ini/language_code.ini')
