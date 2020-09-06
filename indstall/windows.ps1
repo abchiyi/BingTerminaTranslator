e98274f5d02af472292d1d5f566e17b7c0d5ddaf
meme <meme_me2019@outlook.com>
2020-09-06 14:16:07 +0800

    修正的安装脚本

--- indstall/windows.ps1
+++ indstall/windows.ps1
@@ -1,15 +1,9 @@
-# TODO 无效的安装脚本
-$loc = (Get-Location ).Path
-$prg = "C:\Program Files\BinTerminalTranslator"
-# $loc = Split-Path -Parent $MyInvocation.MyCommand.Definition
-
-# Start-Process powershell.exe -Verb runas -ArgumentList '-NoExit','-Command','&{cd (Get-Location).path }'
-# $file_dir = 'C:\Program Files\sspp'
-# Write-Output $prgram_file
-
-
-# Copy-Item $prgram_file $file_dir
-# Start-Process -FilePath powershell.exe -Verb runas -ArgumentList "-noprofile -command "
-
-# $env:Path += ';hello'
-# [System.Environment]::SetEnvironmentVariable("path", $env:Path, "User")
+# 安装位置
+$prg = $HOME + '\bin_terminal_translator'
+# 当前资源位置
+$loc = (Get-Location).Path
+# 复制到指定目录
+Write-Output "复制文件... 请勿关闭"
+Copy-Item $loc $prg -Recurse
+# 添加环境变量
+[System.Environment]::SetEnvironmentVariable("path", $env:Path + ';' + $prg + '\', "User")
