45914647406e3dcdf483529f7ac1021c43c86433
meme <meme_me2019@outlook.com>
2020-09-06 15:35:12 +0800

    修正编码

--- indstall/windows.ps1
+++ indstall/windows.ps1
@@ -1,9 +1,9 @@
-# 安装位置
+# ????
 $prg = $HOME + '\bin_terminal_translator'
-# 当前资源位置
+# ??????
 $loc = (Get-Location).Path
-# 复制到指定目录
-Write-Output "复制文件... 请勿关闭"
+# ???????
+Write-Output "????... ????"
 Copy-Item $loc $prg -Recurse
-# 添加环境变量
+# ??????
 [System.Environment]::SetEnvironmentVariable("path", $env:Path + ';' + $prg + '\', "User")
