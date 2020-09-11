a7eac9b83e99a0dda30d854de3beb12d77b98491
meme <meme_me2019@outlook.com>
2020-09-11 16:45:36 +0800

    修正

--- install/linux.sh
+++ install/linux.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+prg_path=~/bin_terminal_translator/
 python_path=$(which python)
 if (($?!=0));then
     python_path=$(which python3)
@@ -10,21 +11,16 @@ if (($?!=0)); then
     exit 1
 fi
 
-prg_path=~/bin_terminal_translator/
 $python_path -m pip -V
 if (($?!=0)); then
-    echo $?
     echo 没有pip包管理器,请为 $python_path 下的python安装pip包管理器
     exit 1
 fi
-$python_path -m pip install -r $prg_path/install/packegs_of_linux
-if (($?!=0)); then
-    echo $?
-    exit 1
-fi
-mv $prg_path/bin.py $prg_path/co.py
+$python_path -m pip install -r $prg_path'install/packegs_of_linux'
 
-echo "#!$(which bash)" > $prg_path/bin.sh
-echo "$python_path $prg_path'co.py $*'" >> "$prg_path'bin.sh'"
-chmod 700 "$prg_path'bin.sh'"
+mv $prg_path'bin.py' $prg_path'co.py'
+
+echo '#!'$(which bash) > $prg_path/bin.sh
+echo $python_path $prg_path'co.py $*' >> $prg_path'bin.sh'
+chmod 700 $prg_path'bin.sh'
 exit 0
