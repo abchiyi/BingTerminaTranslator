690cb588fbfbb2dd7057d141257c3d03a97e9dd2
meme <meme_me2019@outlook.com>
2020-09-21 00:51:23 +0800

    修正Windows安装脚本，提前创建文件目录

--- install/windows.ps1
+++ install/windows.ps1
@@ -1,6 +1,7 @@
 $prg = $HOME + '\.bin_terminal_translator'
 $loc = (Get-Location).Path
 
+mkdir $prg
 Copy-Item $loc\* $prg -Recurse -Force
 if ($env:Path -cnotmatch ($prg -replace '\\', '\\') ) {
     [System.Environment]::SetEnvironmentVariable(
