091b8c3a72ca75da7b2e4e54da93e0818791a12e
meme <meme_me2019@outlook.com>
2020-09-20 23:00:21 +0800

    修正windows安装脚本

--- install/windows.ps1
+++ install/windows.ps1
@@ -1,4 +1,4 @@
 $prg = $HOME + '\.bin_terminal_translator'
 $loc = (Get-Location).Path
-Copy-Item $loc $prg -Recurse
+Copy-Item $loc\* $prg -Recurse -Force
 [System.Environment]::SetEnvironmentVariable("path", $env:Path + ';' + $prg + '\', "User")
