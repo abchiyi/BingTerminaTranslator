f0a9120db989af02d325932bc68dffcba085d4db
meme <meme_me2019@outlook.com>
2020-09-20 22:45:39 +0800

    修正安装windwos安装脚本目录

--- packeg_for_windows.ps1
+++ packeg_for_windows.ps1
@@ -7,7 +7,7 @@ $packge_name = $HOME + '\Desktop\Windows-x64-' + (git tag)[-1] + '.7z'
 # 复制配置文件
 mkdir .\dist\bin\ini\
 Copy-Item .\ini\* .\dist\bin\ini\
-Copy-Item .\indstall\windows.ps1 .\dist\bin\install.ps1
+Copy-Item .\install\windows.ps1 .\dist\bin\install.ps1
 # 压缩
 & 'C:\Program Files\7-Zip\7z.exe' a -t7z -r $packge_name $loc
 # 清理k
